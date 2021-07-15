752c5423fe5c270506a1e14b350370a2c0fc4312
ernestovalent <ernesto.valen.t@gmail.com>
2021-07-15 14:13:31 -0500

    before webpack

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,6 +18,13 @@ module.exports = function (grunt) {
 				options: {
 					livereload: true
 				}
+			},
+			browserSync: {
+				files: ['src/*.html', 'src/css/*.css'],
+				tasks: ['browserSync'],
+				options: {
+					livereload: true
+				}
 			}
 		},
 
@@ -36,13 +43,35 @@ module.exports = function (grunt) {
 		browserSync: {
 			dev: {
 				bsFiles: {
-					src: ['src/index.html', 'src/css/*.css']
+					src: ['src/*.html', 'src/css/*.css']
 				},
 				options: {
 					watchTask: true,
 					proxy: "localhost:3000",
 					open: false
 				}
+			}
+		},
+
+		// HTML Min
+		htmlmin:{
+			dist: {
+				options: {
+					removeComments:true,
+					collapseWhitespace:true
+				},
+				files: {
+					"dist/vpn.html":"src/vpn.html"
+				}
+			},
+			dev: {
+
+			}
+		},
+
+		autoprefixer: {
+			single: {
+				src: 'src/vpn.css',
 			}
 		},
 
@@ -58,6 +87,18 @@ module.exports = function (grunt) {
 					'src/css/style.css': 'src/sass/principal.sass'
 				}
 			}
+		},
+
+		cssmin: {
+			mini: {
+				files: [{
+					expand: true,
+					cwd: 'src/',
+					src: ['vpn.css'],
+					dest: 'dist/css',
+					ext:'.min.css'
+				}]
+			}
 		}
 
 
@@ -66,7 +107,11 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-browser-sync');
 	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-	grunt.registerTask('sass_compile', ['sass']);
+	grunt.registerTask('html', ['htmlmin','sass_compile']);
+	grunt.registerTask('sass_compile', ['sass','autoprefixer','cssmin']);
 	grunt.registerTask('dev', ['express:dev', 'sass_compile', 'browserSync:dev', 'watch']);
 };
